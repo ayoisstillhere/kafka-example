26b1f3d40b3cb8321617dc1792142a6b3175d874
Ayodele Fagbami <afagbami@gmail.com>
2022-06-30 16:20:24 +0100

    created kafka template

--- src/main/java/com/example/kafkaexample/KafkaApplication.java
+++ src/main/java/com/example/kafkaexample/KafkaApplication.java
@@ -1,7 +1,9 @@
 package com.example.kafkaexample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaApplication {
@@ -10,4 +12,9 @@ public class KafkaApplication {
         SpringApplication.run(KafkaApplication.class, args);
     }
 
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+          kafkaTemplate.send("ayodele", "hello kafka");
+        };
+    }
 }
