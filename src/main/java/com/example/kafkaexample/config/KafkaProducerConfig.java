4925465c0964fae760d4149edd2d4ad574f30231
Ayodele Fagbami <afagbami@gmail.com>
2022-06-30 16:01:06 +0100

    added variable that will hold bootstrap server url

--- src/main/java/com/example/kafkaexample/config/KafkaProducerConfig.java
+++ src/main/java/com/example/kafkaexample/config/KafkaProducerConfig.java
@@ -1,4 +1,11 @@
 package com.example.kafkaexample.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 }
