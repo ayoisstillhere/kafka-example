e4dbc8c01dc01813495b7d1ab196a365769e940a
Ayodele Fagbami <afagbami@gmail.com>
2022-06-30 16:40:31 +0100

    sent 100 messages to kafka and read events in terminal

--- src/main/java/com/example/kafkaexample/KafkaApplication.java
+++ src/main/java/com/example/kafkaexample/KafkaApplication.java
@@ -16,7 +16,9 @@ public class KafkaApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-          kafkaTemplate.send("ayodele", "hello kafka");
+            for (int i = 0; i < 100; i++) {
+                kafkaTemplate.send("ayodele", "hello kafka ;)" + i);
+            }
         };
     }
 }
