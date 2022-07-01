3ea10ef61ba0117a1b41938f4587c7a2b8dfa805
Ayodele Fagbami <afagbami@gmail.com>
2022-07-01 07:27:56 +0100

    set up post mapping

--- src/main/java/com/example/kafkaexample/MessageController.java
+++ src/main/java/com/example/kafkaexample/MessageController.java
@@ -1,5 +1,8 @@
 package com.example.kafkaexample;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/messages")
 public class MessageController {
 
-    public void publish()
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request) {
+        kafkaTemplate.send("ayodele2", request.message());
+    }
 }
